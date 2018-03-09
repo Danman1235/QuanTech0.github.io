372efb81549b1bb41147a0e450445758c28fabae
QuanTech0 <1337h4x0rname@gmail.com>
2018-03-09 14:09:09 -0500

    Update langton.js

--- langton.js
+++ langton.js
@@ -50,8 +50,38 @@ function userDraw(e)
   var x = Math.floor((e.clientX - c.offsetLeft) / 20);
   var y = Math.floor((e.clientY - c.offsetTop) / 20);
   
+  var i = y*20 + x;
+  
+  cells[i] = 1;
+  
   ctx.fillStyle = "#000000";
   ctx.fillRect(x*20, y*20, 20, 20);
+}
+
+drawCells();
+drawGrid();
+
+function update()
+{
+  var temp = new Array(400);
+  
+  for(i=0; i<temp.length; i++)
+  {
+    temp[i] = cells[i];
+  }
+  
+  init = Math.floor(Math.random() * 400); //pick a random cell to start with
+  
+}
+
+function play()
+{
+  var tick = setInterval(update, 1000);
+}
+
+function stop()
+{
+  clearInterval(tick);
 }
 
 c.addEventListener('click', userDraw, false);
